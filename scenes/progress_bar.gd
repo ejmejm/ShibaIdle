bb0a916aa630d875c5aa826d8566999a2f8de00f
BuildTools <unconfigured@null.spigotmc.org>
2024-10-06 18:53:50 -0500

    added progress bar

---
+++ scenes/progress_bar.gd
@@ -0,0 +1,15 @@
+extends ProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_stats_treat_count_update(n_treats) -> void:
+	value = n_treats
