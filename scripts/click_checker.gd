0b6ccdbc98f1613fbead7a72f6129ce32ef3b749
jaffar9 <michaelzadeh@gmail.com>
2024-10-06 22:48:57 -0700

    added container/font style

--- scripts/click_checker.gd
+++ scripts/click_checker.gd
@@ -9,3 +9,4 @@ extends Node2D
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("PrimaryClick"):
 		player_stats.n_treats += treat_give_count
+		
