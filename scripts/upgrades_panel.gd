e04b2e68e2f2a8c2d174903ddeab3fcf62401969
ejmejm <ninjaedan@aol.com>
2024-10-06 21:52:44 -0500

    Control click for +10 upgrades

--- scripts/upgrades_panel.gd
+++ scripts/upgrades_panel.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 
 @onready var upgrades_manager: UpgradeManager = %UpgradesManager
 @onready var button_container: Control = %ButtonContainer
+@onready var upgrades_header: Label = %UpgradesHeaderLabel
 var upgrade_button_template: PackedScene = preload("res://scenes/upgrade_button.tscn")
 
 
@@ -19,12 +20,24 @@ func _on_ready():
 		button_container.add_child(new_button)
 
 
+func _input(event: InputEvent):
+	if event.is_action_pressed("ModifierKey"):
+		upgrades_header.text = "Upgrades (+10)"
+	elif event.is_action_released("ModifierKey"):
+		upgrades_header.text = "Upgrades"
+
+
+func get_buttons() -> Array[UpgradeButton]:
+	var nodes = button_container.get_children()
+	var buttons: Array[UpgradeButton]
+	buttons.assign(nodes)
+	return buttons
+
+
 ## Everytime the timer goes up, show the visible upgrades and hide the others
 func _on_update_timer() -> void:
 	# First remove all previous buttons
 	for node in button_container.get_children():
-		#if not (node.get_script() is UpgradeButton):
-			#continue
 			
 		var button_script: UpgradeButton = node # .get_script()
 		var upgrade := button_script.get_upgrade()
